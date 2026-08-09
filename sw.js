d213dd163bd986cb22aaadc1b3f04ecbc50f6947
Benhur2a <101235301+Benhur2a@users.noreply.github.com>
2026-08-08 22:40:39 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Cache offline da Agenda (PWA).
    Estrategia: rede primeiro (mantem a atualizacao automatica),
    cache como reserva quando estiver sem internet. */
-const CACHE = 'agenda-leo-v3';
+const CACHE = 'agenda-leo-v6';
 const SHELL = [
   './', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png',
